59e23ee20d14b7e5fe14589dfca53a1155cb4965
anna-sahradyan <a.sahradyan72@gmail.com>
2023-01-24 18:08:24 +0400

    add middleware checkAuth file

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 //!Register
 export const register = async (req, res) => {
 
@@ -20,19 +21,74 @@ export const register = async (req, res) => {
         await newUser.save()
         res.json({
             newUser,
-            message:`Registration Was Successful`
+            message: `Registration Was Successful`
         })
 
     } catch (err) {
+        res.json({
 
+            message: `You aren't registration `
+        })
     }
 
 };
 //!Login
 export const login = async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        if (!user) {
+            return res.json({
+                message: `No such user`
+            });
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if (!isPasswordCorrect) {
+            return res.json({
+                message: `incorrect password`
+            })
+        }
+        const token = jwt.sign({
+            id: user._id,
+
+        }, process.env.JWT_SECRET, {expiresIn: `30d`})
+        res.json({
+            token,
+            user,
+
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: "couldn't authorisation"
+        });
+    }
+
 
 };
 //!Get Me
 export const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.userId);
+        if (!user) {
+            return res.json({
+                message: `No such user`
+            });
+        }
+        const token = jwt.sign({
+                id: user._id,
+
+            }, process.env.JWT_SECRET,
+            {expiresIn: `30d`});
+        res.json({
+            user,
+            token,
+            message: "No access"
+        })
+
+    } catch (err) {
+        res.json({
+            message: "No access ",
+        });
+    }
 
 }
--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
 import {getMe, register,login} from "../controllers/auth-controller.js";
+import checkAuth  from "../middleware/checkAuth.js";
 
 const router = new Router();
 router.post(`/register`, register);
-// router.post(`/login`, login);
-// router.get(`/me`, getMe());
+ router.post(`/login`, login);
+router.get(`/me`,checkAuth, getMe);
 export default router;
